d840ac66d02e139f53eaf87ed83ba5486c492b5f
Praise <aypraise19@gmail.com>
2023-01-30 19:57:26 +0100

    javascript2

---
+++ app.js
@@ -0,0 +1,22 @@
+let months = 12 * 90
+age = 25 * 90
+year = 1998 + 90
+days = 30 * 90
+weeks = 48 * 90
+currentYear = 2023;
+
+function lifeinWeeks(){
+    alert("You have 4320 weeks left");
+}
+lifeinWeeks()
+
+function lifeInDays(){
+    alert("You have 32400 days left");
+}
+lifeInDays()
+
+function lifeInMonths(){
+    alert("you have 1080 months left");
+}
+lifeInMonths()
+
